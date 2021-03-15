f4325f2df250e9bf1cc8d9f0eed9b84569883e77
Juan Chavez <jchavez666@gmail.com>
2021-03-15 17:51:52 +0200

    vue amazing

---
+++ 1-data binding/app.js
@@ -0,0 +1,8 @@
+new Vue ({
+    el: '#app',
+    data: {
+        msj: '',
+        numero: 20,
+        url: 'http://www.google.com'
+    }
+});
---
+++ 2-data binding doble via (two way)/app.js
@@ -0,0 +1,14 @@
+new Vue ({
+    el: '#app',
+    data: {
+        animales : {
+            perro:'woof',
+            gato:'miauh',
+            elefante: 'urhhhh'
+        },
+        // animales: ['perro', 'gato','elefante'],
+        sonidos: ['wof','miauh','urrrr'],
+        entrada: 'hola',
+        options: []
+    }
+});
